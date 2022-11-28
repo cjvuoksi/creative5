eee25aedf2d3d4a540c4410f1431ffbcb8d8e2ee
Charles Johnson <charlesjohnson@Charles-MacBook-Pro.local>
2022-11-28 15:54:06 -0700

    conjugation

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -33,6 +33,7 @@ function Quiz() {
         let verb = randomVerb(); 
         console.log(verb); 
         let three = verb.slice(-3); 
+        let two = verb.slice(-2); 
         if (three === "ida" || three === 'idä') {
             let stem = verb.slice(0,-2); 
             let a = verb.slice(-1); 
@@ -66,29 +67,38 @@ function Quiz() {
                 pass_conditional: stem + 't' + a + 'isiin',
                 part_pass_past: stem + 't' + (a === 'a' ? 'u' : 'y'),
                 part_pass_act: stem + 't' + a + 'v' + a,
-                part_past: stem + 'nn' + (a === 'a' ? 'u' : 'y') + 't',
+                part_past: stem + 'n' + (a === 'a' ? 'u' : 'y') + 't',
                 part_act: stem + 'v' + a
             })
         }
-        else if (three === "taa" || three === 'tää') {
+        else if (three === "taa" || three === 'tää' || two === "oa" || two === "ua" || two === 'ea' || two === 'ia') {
             let stem = verb.slice(0,-1); 
             let four = verb.slice(-4); 
             let a = verb.slice(-1); 
             let soft; 
             if (four === "ttaa" || four === "ttää") {
-                soft = verb.slice(0,-3) + verb.slice(-1); 
+                soft = verb.slice(0,-3) + a; 
             } else if (four === "staa" || four === "stää") {
-                soft = verb.slice(0,-1); 
-            } else if (four === "ntaa" || four === "rtaa") {
-                soft = verb.slice(0,-3) + (four[0] === 'n' ? 'n' + verb.slice(-1) : 'r' + verb.slice(-1));  
-            } else {
+                soft = verb.slice(0,-2) + a;  
+            } else if (four === "ntaa" || four === "rtaa" || four === "ltaa") {
+                soft = verb.slice(0,-3) + four[0] + a;
+            } else if (three === "taa" || three === 'tää') {
                 soft = verb.slice(0,-3) + 'd' + verb.slice(-1); 
+            }
+            else {
+                if (four[0] === 's') {
+                    soft = stem
+                }
+                else if (four.slice(0,2) === 'nt' || four.slice(0,2) === 'rt' || four.slice(0,2) === 'lt') {
+
+                }
+                soft = verb.slice(0,-3) + two[0]
             }
             setConj({
                 present: [
                     soft + 'n',
                     soft + 't', 
-                    stem + a,
+                    stem + stem.slice(-1),
                     soft + 'mme',
                     soft + 'tte',
                     stem + 'vat'
@@ -111,16 +121,18 @@ function Quiz() {
                 ],
                 pass_present: soft.slice(0,-1) + 'et' + a + a + 'n',
                 pass_past: soft.slice(0,-1) + 'ettiin',
-                pass_conditional: soft.slic(0,-1) + 'ett' + a + 'isiin',
-                part_pass_past: '',
-                part_pass_act: '',
-                part_past: '',
-                part_act: ''
+                pass_conditional: soft.slice(0,-1) + 'ett' + a + 'isiin',
+                part_pass_past: soft.slice(0,-1) + 'ett' + (a === 'a' ? 'u' : 'y'),
+                part_pass_act: soft.slice(0,-1) + 'ett' + a + 'v' + a,
+                part_past: stem + 'n' + (a === 'a' ? 'u' : 'y') + 't',
+                part_act: stem + 'v' + a
             }); 
         }
-        else if (three === 'sta' || three === 'stä') {
-            let stem = verb.slice(0,-3) + 'se'; 
-            let past = verb.slice(0,-3) + 'si'; 
+        else if (three === 'sta' || three === 'stä' || three === 'lla' || three === 'llä' ||
+            three === 'rra' || three === 'rrä' || three === "nna" || three === "nnä") {
+            let stem = verb.slice(0,-2) + 'e'; 
+            let past = verb.slice(0,-2) + 'i'; 
+            let a = verb.slice(-1); 
             setConj({
                 present: [
                     stem + 'n',
@@ -128,7 +140,7 @@ function Quiz() {
                     stem + stem.slice(-1),
                     stem + 'mme',
                     stem + 'tte',
-                    stem + 'v' + three.slice(-1) + 't'
+                    stem + 'v' + a + 't'
                     ],
                 past: [
                     past + 'n',
@@ -136,7 +148,7 @@ function Quiz() {
                     past,
                     past + 'mme',
                     past + 'tte',
-                    past + 'v' + three.slice(-1) + 't'
+                    past + 'v' + a + 't'
                     
                 ],
                 conditional: [
@@ -145,13 +157,28 @@ function Quiz() {
                     past + 'si',
                     past + 'mme',
                     past + 'tte',
-                    past + 'siv' + three.slice(-1) + 't'
+                    past + 'siv' + a + 't'
                 ],  
-                pass_present: verb + three.slice(-1) + 'n', 
-                pass_past: verb.slice(0,-1) + (three.slice(-1) === 'a' ? 'u' : 'y'),
-                part_act: stem + 'v' + verb.slice(-1)
+                pass_present: verb + a + 'n', 
+                pass_past: stem + 'tiin',
+                pass_conditional: stem + 't' + a + 'isiin',
+                part_pass_past: stem + 't' + (a === 'a' ? 'u' : 'y'),
+                part_pass_act: stem + 't' + a + 'v' + a,
+                part_past: stem + stem.slice(-1) + (a === 'a' ? 'u' : 'y') + 't',
+                part_act: stem + 'v' + a
             }); 
         }
+        else if (three === 'ata' || three === 'ätä' || three === 'eta' || three === 'etä') {
+            
+        }
+    }
+
+    const soften = (verb) => {
+
+    }
+
+    const harden = (verb) => {
+
     }
 
     return(
